987f3743971577d27129c8523a7e75e70ce38e6e
alum <alum@A47E10.infor.com>
2024-02-27 10:48:16 +0100

    commit2:suma

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -8,6 +8,13 @@ public class Calculadora {
         a=1;
         h=1;
     }
+    
+    public double suma(double x, double y){
+        a=x;
+        h=y;
+        
+        return a+h;
+    }
 
 
     public static void main(String[] args) {
