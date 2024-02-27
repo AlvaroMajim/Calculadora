ee0b3637d209e063721784a4c2fd52237ec33d37
alum <alum@A47E10.infor.com>
2024-02-27 09:56:09 +0100

    commit0:inicio

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,6 +1,13 @@
 package calculadora;
 
 public class Calculadora {
+    double a;
+    double h;
+    
+    public Calculadora(){
+        a=1;
+        h=1;
+    }
 
     public static void main(String[] args) {
         
